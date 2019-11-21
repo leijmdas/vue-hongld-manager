d09d862d8678df996ab23fd1390189b5a976ea76
leijmdas@163.com <leijmdas@163.com>
2019-11-21 16:20:45 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <router-view />
-  <!--<div>-->
-    <!--<img src="@/assets/images/hld.jpg" align="middle"/>-->
-  <!--</div>-->
-    <!--<div>-->
-    <!--<img src="@/assets/images/girl.jpg"/>-->
-    <!--</div>-->
+    <!--<router-view />-->
+    <div align="middle"><img src="@/assets/images/welcome.jpg"/></div>
+    <div align="middle">
+      <img src="@/assets/images/hld.jpg"/>
+    </div>
+    <div align="middle">
+      <img src="@/assets/images/wallpaper.png"/>
+    </div>
+    <div style="position:relative;">
+      <img src="@/assets/images/bottom.jpg" width="100%"/>
+      　　
+      <div    style="position:absolute; z-index:2; left:210px; top:120px;  vertical-align:middle;">深圳市宏利达海绵有限公司 版权所有 2019-2020 All CopyRights
+        Reserve.
+      </div>
 
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
